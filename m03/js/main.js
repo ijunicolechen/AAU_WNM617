68a839878faa7c24442b2301653b21ee84baff5b
Nicole Chen <ijuchen32@gmail.com>
2020-02-19 01:03:33 -0800

    Class Demo End

--- m03/js/main.js
+++ m03/js/main.js
@@ -1,7 +1,22 @@
 $(document).ready(() => {
+    // Accordion
     $(".accordion dt").on("click", function (e) {
         console.log($(this));
         // $(this).next().toggle();
-        $(this).next().slideToggle();
+        // $(this).next().slideToggle();
+        // $(this).next().slideDown().siblings("dd").slideUp();
+        if ($(this).hasClass("active")) {
+            $(this).removeClass("active").next().slideUp();
+        } else {
+            $(this).addClass("active").siblings("dt").removeClass("active");
+            $(this).next().slideDown().siblings("dd").slideUp();
+        }
+    })
+    // Tab Group
+    $(".tab").on("click", function (e) {
+        console.log("click");
+        let id = $(this).index();
+        $(this).addClass("active").siblings().removeClass("active");
+        $(this).closest(".tabgroup").find(".content").eq(id).addClass("active").siblings().removeClass("active");
     })
 })
