cdec761460c1f641b2dbcb8fc304fd31f6429c58
ijunicolechen <51040646+ijunicolechen@users.noreply.github.com>
2020-11-04 16:08:33 -0800

    Update connect.php

--- m09/assignment/php/connect.php
+++ m09/assignment/php/connect.php
@@ -7,10 +7,10 @@ error_reporting(E_ALL);
 
 // Connect to DB
 function makeConn() {
-	$host = "localhost";
-	$user = "yuyin617";
-	$pass = "lIxIHYPf4HHw";
-	$dbname = "wnm617Web";
+	$host = "";
+	$user = "";
+	$pass = "";
+	$dbname = "";
 
 	$c = new mysqli($host,$user,$pass,$dbname);
 	if($c->connect_errno) die($c->connect_error);
